5000ca86596a9c90d510429b404d9595ae7418cd
fafa-a <fabienpuech1@gmail.com>
2022-05-05 16:17:12 +0200

    feat(09): setState with unknow value

--- examples/08-character-card/src/Application.tsx
+++ examples/08-character-card/src/Application.tsx
@@ -4,9 +4,25 @@ import { CharacterType, fetchCharacter } from './characters';
 
 import { Loading } from './Loading';
 import { CharacterInformation } from './CharacterInformation';
+import { useEffect, useState } from 'react';
 
 const Application = () => {
-  return <main></main>;
+  const [character, setCharacter] = useState<CharacterType | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      fetchCharacter().then((c) => {
+        setCharacter(c);
+        setLoading(false);
+      });
+    }, 1000);
+  }, []);
+  return (
+    <main>
+      {loading && <Loading />}
+      {character && <CharacterInformation character={character} />}
+    </main>
+  );
 };
 
 export default Application;
